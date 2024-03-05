100c92af7fcbe74fbe6f613fb8d4684a875c9601
lilac <cnjamesdeng@gmail.com>
2024-03-05 23:27:21 +0800

    Update page metadata for SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,25 @@ import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "ChatBar",
-  description: "ChatGPT in the menu Bar",
+  title: "ChatBar - ChatGPT in the menu Bar",
+  description: "Elevate your ChatGPT experience with ChatBar, the ultimate ChatGPT app. Access OpenAI ChatGPT instantly from your menu bar on Windows, Mac, or Linux.",
+  keywords: [
+    "chatgpt",
+    "chat gpt",
+    "chatgpt app",
+    "chat openai",
+    "openai",
+    "open ai",
+    "gpt 4",
+    "ai chat",
+    "ai",
+    "chat",
+    "chatai",
+    "chatbot gpt",
+    "gpt chat",
+    "openai chat",
+    "openai chatgpt"
+  ]
 };
 
 export default function RootLayout({
