14e5b4eeaeeadf5c7478b43b5ce94ffcfbc1050d
lilac <cnjamesdeng@gmail.com>
2024-03-07 23:49:19 +0800

    Add aptabase analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { AptabaseProvider } from '@aptabase/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -38,9 +39,12 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-slate-900 text-slate-300 text-lg min-h-screen overflow-x-hidden`}
       >
-        <Navbar />
-        {children}
-        <Footer />
+        <AptabaseProvider appKey="A-US-4948848664">
+          <Navbar />
+          {children}
+          <Footer />
+        </AptabaseProvider>
+
       </body>
     </html>
   );
