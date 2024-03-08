487ca9f2b8bb77dcda9ecd16e738b13bc306689b
lilac <cnjamesdeng@gmail.com>
2024-03-08 20:52:55 +0800

    Clean code

--- src/components/Analytics.tsx
+++ src/components/Analytics.tsx
@@ -13,7 +13,7 @@ export default function Analytics(props: Props) {
   useEffect(() => {
     console.debug('tracking event:', props.name);
     trackEvent(props.name, props.attrs);
-  }, []); // Empty dependency array ensures this effect runs only once on mount
+  });
 
   return null; // Since this component does not render anything, return null
 }
