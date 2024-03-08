e4de011c65677a7628fb18b7b36b9ea1f6f5ff4c
lilac <cnjamesdeng@gmail.com>
2024-03-08 07:09:53 -0800

    Add google analytics (#6)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -42,6 +43,7 @@ export default function RootLayout({
         {children}
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-XHXGY43X34" />
     </html>
   );
 }
